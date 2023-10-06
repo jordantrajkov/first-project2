5cf307c9ba330fc484f671ae80520bd3f42d22ef
root <root@SKP023003>
2023-10-06 14:18:34 +0200

    Proba za git

---
+++ file.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std ;
+
+
+int main ()
+{   
+    int a = 5;
+    int b = 5;
+
+    int rez = a + b;
+
+    cout << "rez :" <<  rez <<endl;
+
+    return 0;
+}
